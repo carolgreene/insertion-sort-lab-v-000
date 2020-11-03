09ff19211262ab149c08f227ab5f8bf56ae5d0f3
carolgreene <carolgreene3@gmail.com>
2020-11-02 16:02:19 -0800

    added functions

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-function findMinAndRemove(array){
+let unsortedArray = [3, 2, 4, 5, 6, 7]
 
+function findMinAndRemove(array){
+  let min = array[0]
+  let minIndex = 0
+  for(let i = 0; i < array.length; i++) {
+    if(array[i] < min) {
+      min = array[i]
+      minIndex = i
+    }
+  }
+  array.splice(minIndex, 1)
+  console.log(min)
+  return min
 }
 
 function selectionSort(array){
-
+  let newMin;
+  let sorted = []
+  while (array.length != 0) {
+    newMin = findMinAndRemove(array);
+    sorted.push(newMin)
+  }
+  return sorted;
 }
+
+
+
